19ce3bbb0548b64aa75feb7e70da4ff75cb42194
John Dattilo <johnny@Johns-MacBook-Air.local>
2017-04-23 13:43:56 -0500

    Timer done

--- Stopwatch/ViewController.swift
+++ Stopwatch/ViewController.swift
@@ -8,7 +8,12 @@
 
 import UIKit
 
+var count = 0.00
+
+var timer = Timer()
+
 class ViewController: UIViewController {
+    @IBOutlet weak var timerLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,6 +23,28 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    @IBAction func startButton(_ sender: UIButton) {
+        timer = Timer.scheduledTimer(timeInterval: 0.01, target: self, selector: #selector(ViewController.counter), userInfo: nil, repeats: true)
+
+    }
+    @IBAction func pauseButton(_ sender: UIButton) {
+        timer.invalidate()
+
+    }
+    @IBAction func restartButton(_ sender: UIButton) {
+        timer.invalidate()
+        
+        count = 0
+        
+        timerLabel.text = "0.00"
+    }
+    func counter() {
+        
+        count += 0.01
+        
+        timerLabel.text = String(format: "%.02f", count)
+        
     }
 
 
